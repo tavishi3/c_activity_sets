a4a109204171c5eb05601f00c2fba3494943068b
tavishi3 <sinchanau.bsc23@rvu.edu.in>
2024-01-23 10:26:21 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -6,11 +6,11 @@ int main()
     printf("Enter a: ");
     scanf("%d",&a);
     printf("Enter b: ");
-        scanf("%d",&b);
-        int sum;
-        sum=a+b;
-        printf("%d + %d = %d ",a,b,sum);
-        return 0;
+    scanf("%d",&b);
+    int sum;
+    sum=a+b;
+    printf("%d + %d = %d ",a,b,sum);
+    return 0;
 
 }
 
