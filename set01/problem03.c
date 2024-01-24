656a04b2b48f74f87d67e5fa2d57cec547e5d2fe
tavishi3 <sinchanau.bsc23@rvu.edu.in>
2024-01-24 09:52:56 +0530

    test

---
+++ set01/problem03.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+int input()
+{
+    int n;
+    printf("Enter the value: ");
+    scanf("%d",&n);
+    return n;
+}
+int add(int a, int b)
+{
+    int sum;
+    sum=a+b;
+    return sum;
+}
+void output(int a,int b, int sum)
+{
+    printf("%d + %d = %d",a,b,sum);
+}
+int main()
+{
+    int a,b,sum;
+    a=input();
+    b=input();
+    sum=add(a,b);
+    output(a,b,sum);
+     return 0;
+}
